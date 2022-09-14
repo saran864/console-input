19458b54a53e4daf21fc4cca7bd81904216df006
saran864 <112957453+saran864@users.noreply.github.com>
2022-09-14 17:55:21 +0530

    Add files via upload

---
+++ console input.java
@@ -0,0 +1,16 @@
+//print n times the console input
+import java.io.*;
+public class Main
+{
+	public static void main(String  [] args)  throws IOException
+	{
+	int a=Integer.parseInt(args[1]);
+	while(a!=0)
+	{ System.out.print(args[0]+" ");
+	  a--;
+	}
+	
+	}
+}
+
+
